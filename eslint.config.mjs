015af71df019b68ef87ee713f5e9ab1061fa2eb4
Sonam Dechen Yangzom <02230149.cst@rub.edu.bt>
2025-05-20 15:52:09 +0600

    Update ESLint config: turn off @next/next/no-img-element rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,15 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    // Add this object to override or add rules
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  },
+];
 
 export default eslintConfig;
+ 
