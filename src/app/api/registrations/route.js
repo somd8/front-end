f4fb1ca3faedc57f6a5937bdc6948dc430bb23dc
Sonam Dechen Yangzom <02230149.cst@rub.edu.bt>
2025-05-20 15:04:11 +0600

    delete prisma

--- src/app/api/registrations/route.js
+++ src/app/api/registrations/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import prisma from '@/lib/db';
+//import prisma from '@/lib/db';
 import { verifyToken } from '@/lib/auth';
 
 export async function POST(req) {
